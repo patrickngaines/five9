3329f96403e9f3b6b5c0c32f83bdd486ac0e7e52
patrickngaines <patrickngaines@gmail.com>
2025-10-27 13:52:51 -0500

    Add files via upload

---
+++ hideDispoButton.js
@@ -0,0 +1,10 @@
+if(window.location.hostname === "app.five9.com/" {
+    $(#.sfli-dispositions-component).hide();
+}
+/*
+https://drive.google.com/uc?id=1qmc4t5F5gc71EEA5jnpWQSLLHpH3reVm
+
+https://server28.authoritycrm.com/
+
+https://app.five9.com/
+*/
